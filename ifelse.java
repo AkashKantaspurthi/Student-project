0c5d730ef4d370ff31e785657c22b645d66c5020
AkashKantaspurthi <akashkantaspurthi@gmail.com>
2024-07-13 10:46:44 +0530

    updated

--- ifelse.java
+++ ifelse.java
@@ -130,20 +130,6 @@ System.out.println("NO");
 }
 
 
-class Lon
-{
-public static void main(String args[])
-{
-Scanner sc=new Scanner(System.in);
-System.out.println("Enter the year");
-int year;
-year=sc.nextInt();
-if((year%4==0 && year%100!=0) || (year%400==0))
-System.out.println("leap year");
-else
-System.out.println("not aleap year");
-}
-}
 
 
 
